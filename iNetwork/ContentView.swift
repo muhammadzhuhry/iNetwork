35075bac4fb77b12fa181d2d44bca8fa8a007895
muhammadzhuhry <muhammadzhuhry@gmail.com>
2022-08-12 09:02:12 +0700

    Initial Commit

---
+++ iNetwork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iNetwork
+//
+//  Created by Muhammad Zhuhry on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iNetwork/iNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNetworkApp.swift
+//  iNetwork
+//
+//  Created by Muhammad Zhuhry on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
